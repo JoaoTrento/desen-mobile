43731b266f793e5faa32209ffa17a2506b1e82c1
unknown <trentojoao01@gmail.com>
2026-04-03 11:31:30 -0300

    visual e navegação tela de cadastro

--- tela_cadastro/lib/tela_de_cadastro.dart
+++ tela_cadastro/lib/tela_de_cadastro.dart
@@ -32,8 +32,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final controllerNome = TextEditingController();
+  final focusNome = FocusNode();
+
   final controllerIdade = TextEditingController();
+  final focusIdade = FocusNode();
+
   final controllerEmail = TextEditingController();
+  final focusEmail = FocusNode();
 
   bool aceitouTermos = false;
 
@@ -93,53 +98,87 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
             SizedBox(height: 20),
+            Text("Preencha os campos abaixo:",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
             TextField(
+              focusNode: focusNome,
+              textInputAction: TextInputAction.next,
+              onSubmitted: (_) => FocusScope.of(context).requestFocus(focusIdade),
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
                 labelText: "Nome",
                 hintText: "Digite o seu nome",
-                border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
               ),
               controller: controllerNome,
             ),
             SizedBox(height: 20),
             TextField(
+              focusNode: focusIdade,
+              textInputAction: TextInputAction.next,
+              onSubmitted: (_) => FocusScope.of(context).requestFocus(focusEmail),
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
                 labelText: "Idade",
                 hintText: "Digite a sua idade",
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
               controller: controllerIdade,
             ),
             SizedBox(height: 20),
             TextField(
+              focusNode: focusEmail,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
                 labelText: "E-mail",
                 hintText: "Digite o seu e-mail",
-                border: OutlineInputBorder(),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
               controller: controllerEmail,
             ),
             SizedBox(height: 20),
-            DropdownButton<String>(
-              value: sexoUsuario,
-              hint: Text("Selecione o seu sexo"),
-              items: ["Masculino", "Feminino", "Prefiro não informar"]
-                  .map((item) => DropdownMenuItem(
-                value: item,
-                child: Text(item),
-              ))
-                  .toList(),
-              onChanged: (value) {
-                setState(() {
-                  sexoUsuario = value;
-                });
-              },
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: sexoUsuario,
+                  hint: Text("Selecione o seu sexo"),
+                  isExpanded: true,
+                  items: ["Masculino", "Feminino", "Prefiro não informar"]
+                      .map((item) => DropdownMenuItem(
+                    value: item,
+                    child: Text(item),
+                  ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      sexoUsuario = value;
+                    });
+                  },
+                ),
+              ),
             ),
             SizedBox(height: 20),
             Row(
@@ -158,6 +197,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white
+              ),
                 onPressed: () {
                   if (_validarCampos(context)) {
                     Navigator.push(
